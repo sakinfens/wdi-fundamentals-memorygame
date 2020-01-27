f1606296bec0e6efb189fdde2e908886cdfb188e
sakinfens <sakinfens@gmail.com>
2020-01-27 16:48:28 -0500

    created game logic

--- memory_game/JS/client.js
+++ memory_game/JS/client.js
@@ -3,13 +3,19 @@ console.log("Up and running!");
 
 let cards = ['queen','king','king','queen'];
 let cardsInPlay = [];
-let cardOne = cards[0];
-let cardTwo = cards[2];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardsInPlay);
 
-if(cardsInPlay.length === 2){
+function checkForMatch(){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}
+}
+function flipCard(cardId){
+	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId]);
+	checkForMatch();
+	if(cardsInPlay[0] === cardsInPlay[1] ){
 	if(cardsInPlay[0] === cardsInPlay[2]){
 		alert('You found a match');
 	}
@@ -17,3 +23,6 @@ if(cardsInPlay.length === 2){
 		alert('Sorry try again');
 	}
 }
+}
+flipCard(0);
+flipCard(2);
